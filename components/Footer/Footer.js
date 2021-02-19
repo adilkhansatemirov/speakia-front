4184a95a2493074f2ed9675bd2b6df04c43225d0
adilhansatemirovv@gmail.com <adilhansatemirovv@gmail.com>
2021-02-19 12:10:01 +0600

    feat: responsive footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,13 +3,17 @@ function Footer() {
     <footer className="footer">
       <div className="container">
         <div className="footer__text-container">
-          <p>© Speakia.kz 2021</p>
+          <p className="footer__ref-text">© Speakia.kz 2021</p>
           <p className="footer__logo-text">Speakia.kz</p>
-          <p>+ 7 (777) 000-00-00</p>
+          <p className="footer__ref-text">+ 7 (777) 000-00-00</p>
         </div>
         <div className="footer__socials-container">
           <img className="footer__socials-item" src="/images/instagram_icon.svg" alt="instagram" />
           <img className="footer__socials-item" src="/images/whatsapp_icon.svg" alt="instagram" />
+        </div>
+        <div className="footer__ref-mobile-container">
+          <p className="footer__ref-mobile-text">+ 7 (777) 000-00-00</p>
+          <p className="footer__ref-mobile-text">© Speakia.kz 2021</p>
         </div>
       </div>
     </footer>
